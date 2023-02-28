537f7e6376e608f6c154ee98637fdeed477b9b28
pratapsingh19 <itxpratapsingh@gmail.com>
2023-02-28 11:17:32 +0530

    all laravel

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        try {
+            $data = Product::create($request->post());
+
+            return response()->json([
+                'message' => 'Product Created Successfully!!',
+            ]);
+
+        } catch (\Exception$e) {
+
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'Something goes wrong while creating a product!!',
+            ], 500);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
